0d21be3c2cb86a51df3f1dbd968eb63b84cd4300
Nayan Mevada <nayan9229@users.noreply.github.com>
2022-12-25 22:43:13 +0530

    Draft/mystifying goodall (#2)
    * Update 2 files
    
    * Update 4 files

---
+++ src/components/header.js
@@ -0,0 +1,56 @@
+{
+  /* <header class="mb-auto">
+    <div>
+      <h3 class="float-md-start mb-0">Cover</h3>
+      <nav class="nav nav-masthead justify-content-center float-md-end">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="#">Contact</a>
+      </nav>
+    </div>
+  </header> */
+}
+
+export default function header() {
+  return (
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          Navbar
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-link active" aria-current="page" href="#">
+              Home
+            </a>
+            <a class="nav-link" href="#">
+              Features
+            </a>
+            <a class="nav-link" href="#">
+              Pricing
+            </a>
+            <a
+              class="nav-link disabled"
+              href="#"
+              tabindex="-1"
+              aria-disabled="true"
+            >
+              Disabled
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
